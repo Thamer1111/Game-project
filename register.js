cc3f0fc752ed13a4c86efb1564153d5b661afd64
AAbAlQahtani <asmaalqhtani123@gmail.com>
2025-05-14 23:00:32 +0300

    adding register and fixing colors

---
+++ register.js
@@ -0,0 +1,5 @@
+const api = "";
+const name = document.getElementById("name");
+const pass = document.getElementById("pass");
+const confirm = document.getElementById("confirm");
+const btn = document.getElementById("registerBtn");
